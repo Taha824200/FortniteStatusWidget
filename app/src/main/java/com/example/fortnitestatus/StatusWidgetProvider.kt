993bd3ecea67d73898750668ada52e4623c7a77d
Taha824200 <alis900054@gmail.com>
2025-10-20 16:50:16 +0500

    Create StatusWidgetProvider.kt

---
+++ app/src/main/java/com/example/fortnitestatus/StatusWidgetProvider.kt
@@ -0,0 +1,16 @@
+FortniteStatusWidget/
+ ├─ app/
+ │   ├─ build.gradle
+ │   └─ src/
+ │       └─ main/
+ │           ├─ AndroidManifest.xml
+ │           ├─ java/com/example/fortnitestatus/
+ │           │   ├─ StatusWidgetProvider.kt
+ │           │   └─ StatusUpdateService.kt
+ │           ├─ res/
+ │           │   ├─ layout/widget_layout.xml
+ │           │   ├─ drawable/ic_fortnite_logo.png
+ │           │   ├─ values/colors.xml
+ │           │   └─ values/strings.xml
+ │           └─ ...
+ └─ build.gradle (project)
